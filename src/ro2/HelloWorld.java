7f6e836eaa352fb08c5fc18d614614a0466aadcd
jhtr <jhtr@users.noreply.github.com>
2017-03-20 10:45:37 +0100

    Pas coucou

--- src/ro2/HelloWorld.java
+++ src/ro2/HelloWorld.java
@@ -3,7 +3,6 @@ package ro2;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("coucou");
 		System.out.println("test Marie");
 	}
 }
