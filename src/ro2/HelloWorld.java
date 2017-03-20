38f0d5d62cb5a861a5af1096ee5627400707f6ad
ArthurGodet <arth.godet@gmail.com>
2017-03-20 10:45:38 +0100

    suppr helloWorld

--- src/ro2/HelloWorld.java
+++ src/ro2/HelloWorld.java
@@ -2,7 +2,6 @@ package ro2;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
 		System.out.println("coucou");
 		System.out.println("test Marie");
 	}
