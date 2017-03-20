3db49f375d77e526ef25b0fa92cad2d8eac396dd
Marie Deur <marie.deur@gmail.com>
2017-03-20 10:43:05 +0100

    ajout test marie

--- src/ro2/HelloWorld.java
+++ src/ro2/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("coucou");
+		System.out.println("test Marie");
 	}
 }
