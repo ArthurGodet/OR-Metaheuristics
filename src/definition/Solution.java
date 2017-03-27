f385ea77766179817c0b17b3f043fba530f3bed5
jhtr <jhtr@users.noreply.github.com>
2017-03-27 13:51:27 +0200

    documentation de Solution

--- src/definition/Solution.java
+++ src/definition/Solution.java
@@ -1,10 +1,14 @@
 package definition;
 
+/**
+ * Représentation d'une solution du problème de flowshop de permutation
+ */
 public class Solution implements Comparable<Solution>, Cloneable{
 	private int[] order;
 	private int cmax;
 	private Instance instance;
 
+	/** Constructeur */
 	public Solution(Instance i){
 		this.instance = i;
 		this.order = new int[i.getNbJobs()];
@@ -13,18 +17,22 @@ public class Solution implements Comparable<Solution>, Cloneable{
 		this.cmax = Integer.MAX_VALUE;
 	}
 
+	/** @return le job d'indice pos dans l'ordonnancement */
 	public int getJob(int pos){
 		return this.order[pos];
 	}
 
+	/** Assigne une copie de l'ordonnacement o */
 	public void setOrder(int[] o){
 		this.order = o.clone();
 	}
 
+	/** Assigne le job à l'indice pos dans l'ordonnancement */
 	public void setOrder(int job, int pos){
 		this.order[pos] = job;
 	}
 
+	/** @return l'indice du job dans l'ordonnancement */
 	public int getIndex(int job){
 		for(int i = 0; i<this.order.length; i++)
 			if(this.order[i] == job)
@@ -32,14 +40,17 @@ public class Solution implements Comparable<Solution>, Cloneable{
 		return -1;
 	}
 
+	/** @return la durée totale de l'ordonnancement */
 	public int getCmax() {
 		return cmax;
 	}
 
+	/** @return l'instance du problème */
 	public Instance getInstance(){
 		return this.instance;
 	}
 
+	/** recalcule la durée totale de l'ordonnancement */
 	public void evaluate(){
 		int[] dateDisponibilite = new int[this.instance.getNbMachines()];
 		for(int i = 0; i<this.order.length && this.order[i] != -1; i++){
@@ -51,10 +62,12 @@ public class Solution implements Comparable<Solution>, Cloneable{
 		this.cmax = dateDisponibilite[this.instance.getNbMachines()-1];
 	}
 
+	@Override
 	public int compareTo(Solution s) {
 		return Integer.compare(this.getCmax(),s.getCmax());
 	}
 
+	@Override
 	public Solution clone() {
 		Solution c = new Solution(getInstance());
 		c.cmax = this.cmax;
@@ -62,6 +75,7 @@ public class Solution implements Comparable<Solution>, Cloneable{
 		return c;
 	}
 
+	@Override
 	public String toString(){
 		String s = "Cmax = " + this.cmax + "  <---  (";
 		for(int i = 0; i<this.order.length; i++){
@@ -71,18 +85,21 @@ public class Solution implements Comparable<Solution>, Cloneable{
 		return s;
 	}
 
+	/** Insère le job à l'indice pos dans l'ordonnancement */
 	public void insererJob(int job, int pos){
 		for(int k = this.order.length-1; k>pos; k--)
 			this.order[k] = this.order[k-1];
 		this.order[pos] = job;
 	}
 
+	/** Retire le job à l'indice pos dans l'ordonnancement */
 	public void retirerJob(int pos){
 		for(int k = pos; k<this.order.length-1; k++)
 			this.order[k] = this.order[k+1];
 		this.order[this.order.length-1] = -1;
 	}
 
+	/** @return true ssi le job est présent dans l'ordonnancement */
 	public boolean contains(int job){
 		for(int i = 0; i<this.order.length; i++)
 			if(this.order[i]==job)
@@ -90,14 +107,18 @@ public class Solution implements Comparable<Solution>, Cloneable{
 		return false;
 	}
 
-	// echange les jobs aux positions pos1 et pos2
+	/** Echange les jobs aux indices pos1 et pos2 */
 	public void swap(int pos1, int pos2){
 		int job = this.order[pos1];
 		this.order[pos1] = this.order[pos2];
 		this.order[pos2] = job;
 	}
 
-	// cycle de permutation des jobs : job1 en pos2, job2 en pos3 et job3 en pos1
+	/**
+	 * Effectue une permutation circulaire des jobs aux positions pos1, pos2, pos3.
+	 *
+	 * Le job précédemment en pos1 va en pos2, celui en pos2 va en pos3 et celui en pos3 va en pos1.
+	 */
 	public void change(int pos1, int pos2, int pos3){
 		int job2 = this.order[pos2];
 
@@ -106,35 +127,51 @@ public class Solution implements Comparable<Solution>, Cloneable{
 		this.order[pos3] = job2;
 	}
 
-	// decalage vers la droite d'une chaine de jobs : jobEnd en posBegin, jobBegin en posBegin+1, ... jobEnd-1 en posEnd
+	/**
+	 * Effectue un décalage vers la droite d'une chaîne de jobs.
+	 *
+	 * Le job en position posEnd va en posBegin,
+	 * celui en position posBegin va en posBegin+1,
+	 * ...,
+	 * celui en position jobEnd-1 va en jobEnd.
+	 */
 	public void rightShift(int posBegin, int posEnd){
 		int job = this.order[posEnd];
 		for(int i = posEnd; i>posBegin; i--)
 			this.order[i] = this.order[i-1];
 		this.order[posBegin] = job;
 	}
-	
-	// decalage vers la gauche d'une chaine de jobs : jobEnd en posBegin, jobBegin en posBegin+1, ... jobEnd-1 en posEnd
-		public void leftShift(int posBegin, int posEnd){
-			int job = this.order[posBegin];
-			for(int i = posBegin; i<posEnd; i++)
-				this.order[i] = this.order[i+1];
-			this.order[posEnd] = job;
-		}
 
+	/**
+	 * Effectue un décalage vers la gauche d'une chaîne de jobs.
+	 *
+	 * Le job en position posBegin va en posEnd,
+	 * celui en position posEnd va en posEnd-1,
+	 * ...,
+	 * celui en position posBegin+1 va en posBegin.
+	 */
+	public void leftShift(int posBegin, int posEnd){
+		int job = this.order[posBegin];
+		for(int i = posBegin; i<posEnd; i++)
+			this.order[i] = this.order[i+1];
+		this.order[posEnd] = job;
+	}
+
+	/** @return le meilleur voisin pour le voisinage nh */
 	public Solution generateBestNeighbor(Neighborhood nh){
 		Solution neighbor;
 		switch(nh){
-		case SWAP :
-			neighbor = generateBestNeighborSwap(); break;
-		case CHANGE :
-			neighbor = generateBestNeighborChange(); break;
-		default :
-			neighbor = generateBestNeighborShift(); break;
+			case SWAP :
+				neighbor = generateBestNeighborSwap(); break;
+			case CHANGE :
+				neighbor = generateBestNeighborChange(); break;
+			default :
+				neighbor = generateBestNeighborShift(); break;
 		}
 		return neighbor;
 	}
 
+	/** @return le meilleur voisin dans le voisinage des swaps */
 	private Solution generateBestNeighborSwap(){
 		Solution currentNeighbor = this.clone();
 		Solution bestNeighbor = this.clone();
@@ -149,6 +186,7 @@ public class Solution implements Comparable<Solution>, Cloneable{
 		return bestNeighbor;
 	}
 
+	/** @return le meilleur voisin dans le voisinage des permutations circulaires de trois jobs */
 	private Solution generateBestNeighborChange(){
 		Solution currentNeighbor = this.clone();
 		Solution bestNeighbor = this.clone();
@@ -165,6 +203,7 @@ public class Solution implements Comparable<Solution>, Cloneable{
 		return bestNeighbor;
 	}
 
+	/** @return le meilleur voisin dans le voisinage des décalages à droite */
 	private Solution generateBestNeighborShift(){
 		Solution currentNeighbor = this.clone();
 		Solution bestNeighbor = this.clone();
