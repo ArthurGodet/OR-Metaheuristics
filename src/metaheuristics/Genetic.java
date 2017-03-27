ea9b766ce68b793cade89af7071c4c6fd826ba58
jhtr <jhtr@users.noreply.github.com>
2017-03-28 00:46:35 +0200

    Relecture et commentaires pour les différents crossovers

--- src/metaheuristics/Genetic.java
+++ src/metaheuristics/Genetic.java
@@ -39,120 +39,105 @@ public class Genetic extends Solver{
 	}
 
 	public Solution onePointCrossover(Solution parent1, Solution parent2){
-		int coupure = 0;
-		do{
-			coupure = (int)(Math.random()*this.getInstance().getNbJobs());
-		}while(coupure == parent1.getInstance().getNbJobs()-1);
-
 		Solution child = new Solution(this.getInstance());
 
-		for(int i=0;i<coupure ; i++){
+		int coupure = 1 + (int)(Math.random()*(this.getInstance().getNbJobs()-1)); // entre 1 et nbJobs-1 inclus
+
+		// ajoute les premiers jobs du parent1
+		for(int i=0; i<coupure; i++)
 			child.setOrder(parent1.getJob(i), i);
-		}
+
+		// ajoute les jobs restants dans le même ordre que dans parent2
 		int indexAInserer = coupure;
-		for(int i=0 ; i<parent2.getInstance().getNbJobs(); i++){
-			if(!child.contains(parent2.getJob(i))){
-				child.setOrder(parent2.getJob(i), indexAInserer);
-				indexAInserer++;
-			}
-		}
+		for(int i=0; i<this.getInstance().getNbJobs(); i++)
+			if(!child.contains(parent2.getJob(i)))
+				child.setOrder(parent2.getJob(i), indexAInserer++);
+
 		child.evaluate();
 		return child;
 	}
 
 	public Solution twoPointCrossoverSepares(Solution parent1, Solution parent2){
+		Solution child = new Solution(this.getInstance());
+
 		int coupureDebut=0;
 		int coupureFin=0;
 		do{
-			coupureDebut = (int)(Math.random()*this.getInstance().getNbJobs());
-			coupureFin = (int)(Math.random()*this.getInstance().getNbJobs());
-		}while(coupureDebut==0 && coupureFin==parent2.getInstance().getNbJobs()-1 || coupureDebut == coupureFin);
+			coupureDebut = 1 + (int)(Math.random()*this.getInstance().getNbJobs()-2); // entre 1 et nbJobs-2 inclus
+			coupureFin = 2 + (int)(Math.random()*this.getInstance().getNbJobs()-2); // entre 2 et nbJobs-1 inclus
+		}while(coupureDebut >= coupureFin);
 
-		Solution child = new Solution(this.getInstance());
+		// ajoute les premiers jobs du parent1
+		for(int i=0; i<coupureDebut; i++)
+			child.setOrder(parent1.getJob(i), i);
+		// ajoute les derniers jobs du parent1
+		for(int i=coupureFin; i<this.getInstance().getNbJobs(); i++)
+			child.setOrder(parent1.getJob(i), i);
 
-		for(int i=0; i<coupureDebut; i++){
-			child.setOrder(parent1.getJob(i), i);
-		}
-		for(int i=coupureFin; i<parent2.getInstance().getNbJobs(); i++){
-			child.setOrder(parent1.getJob(i), i);
-		}
+		// ajoute les jobs restants dans le même ordre que dans parent2
 		int indexAInserer = coupureDebut;
-		for(int i=0; i<parent2.getInstance().getNbJobs(); i++){
-			if(!child.contains(parent2.getJob(i))){
-				child.setOrder(parent2.getJob(i), indexAInserer);
-				indexAInserer++;
-			}
-		}
+		for(int i=0; i<this.getInstance().getNbJobs(); i++)
+			if(!child.contains(parent2.getJob(i)))
+				child.setOrder(parent2.getJob(i), indexAInserer++);
+
 		child.evaluate();
 		return child;
 	}
 
 	public Solution twoPointCrossoverEnsemble(Solution parent1, Solution parent2){
+		Solution child = new Solution(this.getInstance());
+
 		int coupureDebut = 0;
 		int coupureFin = 0;
 		do{
-			coupureDebut = (int)(Math.random()*this.getInstance().getNbJobs());
-			coupureFin = (int)(Math.random()*this.getInstance().getNbJobs());
+			coupureDebut = 1 + (int)(Math.random()*this.getInstance().getNbJobs()-2); // entre 1 et nbJobs-2 inclus
+			coupureFin = 2 + (int)(Math.random()*this.getInstance().getNbJobs()-2); // entre 2 et nbJobs-1 inclus
+		}while(coupureDebut >= coupureFin);
 
-		}while(coupureDebut==0 && coupureFin==parent2.getInstance().getNbJobs()-1 || coupureDebut == coupureFin);
-
-		Solution child = new Solution(this.getInstance());
-
-		for(int i=coupureDebut; i<coupureFin; i++){
+		// ajoute les jobs centraux du parent1
+		for(int i=coupureDebut; i<coupureFin; i++)
 			child.setOrder(parent1.getJob(i), i);
-		}
 
+		// ajoute les jobs restants dans le même ordre que dans parent2
 		int indexAInserer = 0;
-		for(int i=0; i<parent2.getInstance().getNbJobs(); i++){
+		for(int i=0; i<this.getInstance().getNbJobs(); i++){
 			if(!child.contains(parent2.getJob(i))){
-				if(indexAInserer < coupureDebut){
-					child.setOrder(parent2.getJob(i), indexAInserer);
-					indexAInserer++;
-				}
-				else{
-					indexAInserer = Math.max(coupureFin, indexAInserer);
-					child.setOrder(parent2.getJob(i), indexAInserer);
-					indexAInserer++;
-				}
-
+				child.setOrder(parent2.getJob(i), indexAInserer++);
+				if(indexAInserer == coupureDebut)
+					indexAInserer = coupureFin;
 			}
 		}
+
 		child.evaluate();
 		return child;
 	}
 
 	public Solution positionBasedCrossover(Solution parent1, Solution parent2){
-		int nbCoupure = 0;
-		do{
-			nbCoupure = (int)(Math.random()*this.getInstance().getNbJobs());
-		}while(nbCoupure==0 || nbCoupure == parent1.getInstance().getNbJobs()-1);
-
-		ArrayList<Integer> coupures = new ArrayList<Integer>();
-		int indexCoupure=0;
-		for(int i=0;i<nbCoupure ; i++){
-			do{
-				indexCoupure = (int)(Math.random()*parent1.getInstance().getNbJobs());
-			}while(coupures.contains(indexCoupure));
-			coupures.add(indexCoupure);
-		}
-
 		Solution child = new Solution(this.getInstance());
 
-		for(Integer i : coupures)
+		int nbHerites = 1 + (int)(Math.random()*(this.getInstance().getNbJobs()-1)); // entre 1 et nbJobs-1 inclus
+
+		List<Integer> indexHerites = new ArrayList<Integer>();
+		for(int i = 0; i < this.getInstance().getNbJobs(); i++)
+			indexHerites.add(i);
+		Collections.shuffle(indexHerites);
+
+		// ajoute les jobs aux index choisis depuis parent1
+		for(Integer i : indexHerites.subList(0, nbHerites))
 			child.setOrder(parent1.getJob(i),i);
 
-		int pos = 0;
-		for(int i=0; i<parent2.getInstance().getNbJobs(); i++){
+		// ajoute les jobs restants dans le même ordre que dans parent2
+		int indexAInserer = 0;
+		for(int i=0; i<this.getInstance().getNbJobs(); i++){
 			if(!child.contains(parent2.getJob(i))){
-				while(child.getJob(pos)!=-1){
-					pos++;
-				}
-				child.setOrder(parent2.getJob(i), pos);	
+				while(child.getJob(indexAInserer)!=-1)
+					indexAInserer++;
+				child.setOrder(parent2.getJob(i), indexAInserer);
 			}
 		}
+
 		child.evaluate();
 		return child;
-
 	}
 
 	public Solution mutation(Solution child){
