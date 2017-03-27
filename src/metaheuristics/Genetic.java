885dc07e273bbad2d86c306f8b653e0a9c0065db
Marie Deur <marie.deur@gmail.com>
2017-03-27 11:10:14 +0200

    ajout methode onePointCrossover

--- src/metaheuristics/Genetic.java
+++ src/metaheuristics/Genetic.java
@@ -1,12 +1,30 @@
 package metaheuristics;
 
 import definition.Instance;
+import definition.Solution;
 
 public class Genetic extends Solver{
 
 	public Genetic(Instance inst) {
 		super(inst);
-		// TODO Auto-generated constructor stub
+	}
+	
+	public Solution onePointCrossover (Solution parent1, Solution parent2){
+		int coupure = (int) Math.random()*super.getInstance().getNbJobs();
+		Solution child = new Solution(super.getInstance());
+		
+		for(int i=0;i<coupure ; i++){
+			child.setOrder(parent1.getJob(i), i);
+		}
+		int indexAInserer = coupure;
+		for(int i=0 ; i<parent2.getInstance().getNbJobs() ;i++){
+			if( !child.contains(parent2.getJob(i))){
+				child.setOrder(parent2.getJob(i), indexAInserer);
+				indexAInserer++;
+			}
+		}
+		child.evaluate();
+		return child;
 	}
 
 	@Override
