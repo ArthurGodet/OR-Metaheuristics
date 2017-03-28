ffbdf2a899c976d56d2bfd8797158e944015af35
jhtr <jhtr@users.noreply.github.com>
2017-03-29 00:56:23 +0200

    Compléter la classe Random avec randomCouple

--- src/util/Random.java
+++ src/util/Random.java
@@ -14,18 +14,19 @@ public class Random {
 		return from + (int)(Math.random()*(to - from));
 	}
 
-	/**
-	 * @return deux entiers aléatoires formant une segment semi-ouvert non vide
-	 * contenu dans l'intervalle entre from inclus et to exclus en laissant
-	 * au moins un élément avant et après.
-	 */
-	public static int[] randomTwoPoints(int from, int to) {
-		int i = randomInteger(from+1, to);
-		int j = randomInteger(from+2, to);
-		if(i < j)
-			return new int[]{i, j};
+	/** @return un couple aléatoire d'entiers (i,j) avec i < j entre from inclus et to exclus */
+	public static int[] randomCouple(int from, int to) {
+		int i = randomInteger(from, to);
+		int j = randomInteger(from, to-1);
+		if(i <= j)
+			return new int[]{i, j+1};
 		else
-			return new int[]{j-1, i};
+			return new int[]{j, i};
+	}
+
+	/** @return un couple aléatoire d'entiers (i,j) avec i < j entre from exclus et to exclus */
+	public static int[] randomTwoPoints(int from, int to) {
+		return randomCouple(from+1, to);
 	}
 
 	/** @return une liste de k entiers aléatoires sans doublons entre from inclus et to exclus */
@@ -35,18 +36,5 @@ public class Random {
 			l.add(i);
 		Collections.shuffle(l);
 		return l.subList(0, k);
-	}
-
-	/** Affichage des valeurs calculées par randomTwoPoints pour se convaincre de son fonctionnement */
-	public static void main(String[] args) {
-		int from = 0, to = 5;
-		for(int i = from+1; i < to; i++) {
-			for(int j = from+2; j < to; j++)
-				if(i < j)
-					System.out.print(" "+i+","+j);
-				else
-					System.out.print(" "+(j-1)+","+i);
-			System.out.println();
-		}
 	}
 }
