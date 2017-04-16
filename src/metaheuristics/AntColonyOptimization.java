7994944ce28514a0b8c72c486c2550cd02fadc4d
ArthurGodet <arth.godet@gmail.com>
2017-04-16 15:56:23 +0200

    ajout du evaluate() dans Ant

--- src/metaheuristics/AntColonyOptimization.java
+++ src/metaheuristics/AntColonyOptimization.java
@@ -118,6 +118,7 @@ public class AntColonyOptimization extends Solver {
 					candidates.remove(candidateSelected);
 					pos++;
 				}
+				ants[k].evaluate();
 			}
 			updatePheromoneTrails();
 		}
