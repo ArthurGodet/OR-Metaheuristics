4ac1aefcd2d0e8798a313b0a04838b46d11a354d
ArthurGodet <arth.godet@gmail.com>
2017-04-16 19:01:58 +0200

    restructuration des resultats

--- src/metaheuristics/Benchmark.java
+++ src/metaheuristics/Benchmark.java
@@ -5,19 +5,19 @@ import java.io.IOException;
 
 public class Benchmark {
 	public static void writeResults(Solver solver) throws IOException{
-		FileWriter fw = new FileWriter("results/"+solver.getName()+".txt",true);
+		FileWriter fw = new FileWriter("results/"+System.getProperty("user.name")+"/"+solver.getName()+".txt",true);
 		fw.write(solver.getSolution().getCmax() + "\n");
 		fw.close();
 	}
 	
 	public static void presentation(Solver solver, String instance) throws IOException{
-		FileWriter fw = new FileWriter("results/"+solver.getName()+".txt",true);
+		FileWriter fw = new FileWriter("results/"+System.getProperty("user.name")+"/"+solver.getName()+".txt",true);
 		fw.write(instance+"\n");
 		fw.close();
 	}
 	
 	public static void EndPresentation(Solver solver) throws IOException{
-		FileWriter fw = new FileWriter("results/"+solver.getName()+".txt",true);
+		FileWriter fw = new FileWriter("results/"+System.getProperty("user.name")+"/"+solver.getName()+".txt",true);
 		fw.write("-----------"+"\n");
 		fw.close();
 	}
